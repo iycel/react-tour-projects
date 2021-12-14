7f22a9497ff29e27054e5b443f61170e0d43e599
iycel <ismailycel@gmail.com>
2021-12-14 21:16:02 +0300

    basic tour_projects final

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import './App.css';
 import Header from './components/header/Header';
 import Navbar from './components/navbar/Navbar';
 import Cards from './components/cards/Cards';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [isVisible, setIsVisible] = useState(false);
+  const onMouseClick = () => {
+    setIsVisible(!isVisible)
+  };
+
   return (
     <div className='App'>
       < Navbar />
       < Header />
-      < Cards />
+      < Cards isVisible={isVisible} onMouseClick={onMouseClick} />
     </div >
   );
 };
--- src/components/cards/Cards.js
+++ src/components/cards/Cards.js
@@ -2,24 +2,63 @@ import React from 'react';
 import { data } from '../../helpers/Data';
 import './Cards.css';
 
-const Cards = () => {
+// const Cards = () => {
+//     const [showImage, setShowImage] = useState(true)
+//     return (
+//         <div className='card-container' onClick={() => setShowImage(!showImage)}>
+//             {showImage ? (
+//                 <>
+//                     {data.map(
+//                         (card, index) => {
+//                             return (
+//                                 <div className='cards' key={index}>
+//                                     <img src={card.image} alt={card.title} />
+//                                 </div>
+//                             )
+//                         }
+//                     )}
+//                 </>
+//             ) : (
+//                 <>
+//                     {data.map(
+//                         (card, index) => {
+//                             return (
+//                                 <div className='cards' key={index}>
+//                                     <div className='title'>
+//                                         <h2>{card.title}</h2>
+//                                     </div>
+//                                     <img src={card.image} alt={card.title} />
+//                                     <div className='card-over'>
+//                                         <p>{card.desc}</p>
+//                                     </div>
+//                                 </div>
+//                             )
+//                         }
+//                     )}
+//                 </>
+//             )}
+//         </div>
+//     );
+// };
+
+// export default Cards;
+
+const Cards = (props) => {
     return (
-        <div className='card-container'>
-            {data.map(
-                (card, index) => {
-                    return (
-                        <div className='cards' key={index}>
-                            <div className='title'>
-                                <h2>{card.title}</h2>
-                            </div>
-                            <img src={card.image} alt={card.title} />
-                            <div className='card-over'>
-                                <p>{card.desc}</p>
-                            </div>
+        <div className='card-container' >
+            {data.map((card, index) => {
+                return (
+                    <div className='cards' key={index} onClick={props.onMouseClick}>
+                        <div className='title'>
+                            <h2>{card.title}</h2>
                         </div>
-                    )
-                }
-            )}
+                        <img src={card.image} alt={card.title} />
+                        <div className={props.isVisible ? 'card-over' : 'card-over-no'}>
+                            <p>{card.desc}</p>
+                        </div>
+                    </div>
+                );
+            })}
         </div>
     );
 };
